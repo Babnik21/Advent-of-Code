acd58c18814110809679a5bc563f3a0adaa82c46
Babnik21 <jure.babnik.21@gmail.com>
2020-12-03 10:17:32 +0100

    day_3 done! :)

--- day_3/day_3.ml
+++ day_3/day_3.ml
@@ -24,16 +24,21 @@ let rec count_trees_inline list_1 step start skip =
   if skip = 1 then
     match list_1 with
     | [] -> 0
-    | x :: rest -> if x.[start] = '#' then 1 + count_trees_inline rest step ((start + step) mod String.length x) skip else count_trees_inline rest step ((start + step) mod String.length x) skip
-  else 
+    | x :: [] -> if x.[start] = '#' then 1 else 0
+    | x :: skipped :: rest -> if x.[start] = '#' then 1 + count_trees_inline rest step ((start + step) mod String.length x) skip else count_trees_inline rest step ((start + step) mod String.length x) skip
+    else 
     match list_1 with 
     | [] -> 0
     | x :: rest -> if x.[start] = '#' then 1 + count_trees_inline rest step ((start + step) mod String.length x) skip else count_trees_inline rest step ((start + step) mod String.length x) skip
   
+(* Rešitev 2. naloge: zmnožimo med seboj, ko pridemo do konca še primer ko preskakujemo vsako drugo vrstico *)
 
+let rec prod_trees list_1 traj_list = match traj_list with
+  | [] -> count_trees_inline list_1 1 0 1
+  | x :: rest -> (count_trees_inline list_1 x 0 0) * prod_trees list_1 rest 
 
-let solution_1 = count_trees_inline (read_file in_file) 3 0
-let solution_2 = 2
+let solution_1 = count_trees_inline (read_file in_file) 3 0 0
+let solution_2 = prod_trees (read_file in_file) [1; 3; 5; 7]
 
 
 
